fd7be36544446a59adc1d1d65f65d48d74802d7d
Marcelo Wiermann <marcelo@wiermann.me>
2020-11-16 18:44:35 +0100

    fix: prevent crash with 0 blocks

--- src/controllers/ProofsController.ts
+++ src/controllers/ProofsController.ts
@@ -16,9 +16,14 @@ class ProofsController {
 
   index = async (request: Request, response: Response): Promise<void> => {
     const block = await Block.findOne({status: 'finalized'}).sort({height: -1}).limit(1);
-    const keys = request.query.keys as string[];
-    const leaves = await Leaf.find({blockId: block.id, key: {$in: keys}});
-    response.send({ data: {block: block, keys: keys, leaves: leaves} });
+
+    if (block) {
+      const keys = request.query.keys as string[];
+      const leaves = await Leaf.find({blockId: block.id, key: {$in: keys}});
+      response.send({ data: {block: block, keys: keys, leaves: leaves} });
+    } else {
+      response.send({ data: {} });
+    }
   }
 }
 
