27887e8621e94dbf5a65a8fb1bbd49f9aa947b52
DZariusz <dzariusz@gmail.com>
2020-11-30 12:42:18 +0100

    feat: use toolbox to convert leaf value to buffer

--- src/services/SortedMerkleTreeFactory.ts
+++ src/services/SortedMerkleTreeFactory.ts
@@ -1,5 +1,5 @@
 import {injectable} from 'inversify';
-import int64 from 'int64-buffer';
+import {LeafType, LeafValueCoder} from '@umb-network/toolbox';
 
 import SortedMerkleTree from '../lib/SortedMerkleTree';
 
@@ -9,18 +9,12 @@ class SortedMerkleTreeFactory {
     const treeData = Array.from(data.keys()).sort()
       .map(key => {
         const value = data.get(key);
-        const converted = this.intToBuffer(Number(value));
+        const converted = LeafValueCoder.encode(Number(value), LeafType.TYPE_FLOAT);
         return {[key]: converted};
       })
       .reduce((acc, v) => ({...acc, ...v}), {});
 
     return new SortedMerkleTree(treeData);
-  }
-
-  private intToBuffer(i: number): Buffer {
-    const hex = new int64.Int64BE(i).toBuffer().toString('hex')
-    const hexInt = hex.replace(/^0+/g, '')
-    return Buffer.from(`${hexInt.length % 2 === 0 ? '' : '0'}${hexInt}`, 'hex')
   }
 }
 
