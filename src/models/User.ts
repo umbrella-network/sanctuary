83073b3bf432f9745c05399891718f8310629977
Noah Matisoff <noahmatisoff@hey.com>
2020-12-21 15:02:46 -0800

    setup company model along with project

--- src/models/User.ts
+++ src/models/User.ts
@@ -8,7 +8,7 @@ export interface IUser extends Document {
 const UserSchema: Schema = new Schema({
   _id: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  password: { type: String, required: true }
 });
 
 UserSchema.index({ email: 1 });
