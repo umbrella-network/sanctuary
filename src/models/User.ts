717d284359b5b4cb91c5e9d6a2dc4e523f72c0da
Noah Matisoff <noahmatisoff@hey.com>
2020-12-24 10:25:49 -0800

    add verified field

--- src/models/User.ts
+++ src/models/User.ts
@@ -9,6 +9,7 @@ const UserSchema: Schema = new Schema({
   _id: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  verified: { type: Boolean, required: true, default: false },
 });
 
 UserSchema.index({ email: 1 }, { unique: true });
