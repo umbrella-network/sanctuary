8b27afd2ba201febb1b6f9d50a4acc093fcaceed
Noah Matisoff <noahmatisoff@hey.com>
2021-01-01 14:44:17 -0800

    define type for token

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,11 @@
 import * as jwt from 'jsonwebtoken';
 
-export function getAuthorizationToken(authorizationHeader: string): object | undefined {
+interface Token {
+  userId: number;
+  exp: number;
+}
+
+export function getAuthorizationToken(authorizationHeader: string): Token | undefined {
   const token = authorizationHeader.replace('Bearer ', '');
   try {
     const decoded = jwt.verify(token, process.env.AUTH_PRIVATE_KEY);
