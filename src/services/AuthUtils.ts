2cbee2c888555948b003b1eabe97687f09ab9d7e
Edgar <starguitar307@gmail.com>
2021-01-08 00:49:25 +0200

    feat: require api keys for GET /proofs

---
+++ src/services/AuthUtils.ts
@@ -0,0 +1,28 @@
+import { injectable } from 'inversify';
+import ApiKey from '../models/ApiKey';
+import { IApiKey } from '../models/ApiKey';
+
+@injectable()
+export class AuthUtils {
+  async verifyApiKeyFromAuthHeader(
+    authorizationHeader?: string,
+  ): Promise<{ apiKey: IApiKey; errorMessage?: void } | { apiKey?: void; errorMessage: string }> {
+    if (!authorizationHeader) {
+      return { errorMessage: 'No authorization header' };
+    }
+
+    const key = authorizationHeader.replace('Bearer ', '');
+
+    const apiKey = await ApiKey.findOne({ key });
+
+    if (!apiKey) {
+      return { errorMessage: 'Unknown API key' };
+    }
+
+    if (apiKey.expiresAt && apiKey.expiresAt.valueOf() < new Date().valueOf()) {
+      return { errorMessage: 'API key is expired' };
+    }
+
+    return { apiKey };
+  }
+}
