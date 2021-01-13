1fc937861ec91e29bb3ff27eb1938d1f35c7b8a4
Edgar <starguitar307@gmail.com>
2021-01-13 02:32:52 +0200

    feat(api-keys): require api key for blocks, leaves and keys

--- src/controllers/BlocksController.ts
+++ src/controllers/BlocksController.ts
@@ -1,17 +1,27 @@
-import { injectable } from 'inversify';
+import { inject, injectable } from 'inversify';
 import express, { Request, Response } from 'express';
 import Block from '../models/Block';
 import Leaf from '../models/Leaf';
+import { AuthUtils } from '../services/AuthUtils';
 
 @injectable()
 class BlocksController {
   router: express.Application;
 
-  constructor() {
+  constructor(
+    @inject(AuthUtils) private readonly authUtils: AuthUtils,
+  ) {
     this.router = express().get('/', this.index).get('/:id', this.show).get('/:id/leaves', this.leaves);
   }
 
   index = async (request: Request, response: Response): Promise<void> => {
+    const apiKeyVerificationResult = await this.authUtils.verifyApiKeyFromAuthHeader(request.headers.authorization);
+
+    if (!apiKeyVerificationResult.apiKey) {
+      response.status(401).send({ error: apiKeyVerificationResult.errorMessage });
+      return;
+    }
+
     const offset: number = parseInt(<string>request.query.offset) || 0;
     const limit: number = parseInt(<string>request.query.limit) || 100;
 
@@ -21,11 +31,25 @@ class BlocksController {
   };
 
   show = async (request: Request, response: Response): Promise<void> => {
+    const apiKeyVerificationResult = await this.authUtils.verifyApiKeyFromAuthHeader(request.headers.authorization);
+
+    if (!apiKeyVerificationResult.apiKey) {
+      response.status(401).send({ error: apiKeyVerificationResult.errorMessage });
+      return;
+    }
+
     const block = await Block.findById(request.params.id);
     response.send({ data: block });
   };
 
   leaves = async (request: Request, response: Response): Promise<void> => {
+    const apiKeyVerificationResult = await this.authUtils.verifyApiKeyFromAuthHeader(request.headers.authorization);
+
+    if (!apiKeyVerificationResult.apiKey) {
+      response.status(401).send({ error: apiKeyVerificationResult.errorMessage });
+      return;
+    }
+
     const leaves = await Leaf.find({ blockId: request.params.id });
     response.send(leaves);
   };
