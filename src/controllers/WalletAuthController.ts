96a100152dc309753809133e7558a69a94daf3b2
Noah Matisoff <noahmatisoff@hey.com>
2021-01-14 21:26:52 -0800

    use timestamp based wallet auth and return jwt

--- src/controllers/WalletAuthController.ts
+++ src/controllers/WalletAuthController.ts
@@ -1,41 +1,34 @@
-import { injectable } from 'inversify';
+import { inject, injectable } from 'inversify';
 import express, { Request, Response } from 'express';
 import { ethers } from 'ethers';
-import cryptoRandomString from 'crypto-random-string';
-import WalletAuth from '../models/WalletAuth';
-import { add } from 'winston';
+import { sign } from 'jsonwebtoken';
+import Settings from '../types/Settings';
 
 @injectable()
 class WalletAuthController {
   router: express.Router;
+  settings: Settings;
 
-  constructor() {
-    this.router = express.Router().post('/', this.create).post('/verify', this.verify);
+  constructor(@inject('Settings') settings: Settings) {
+    this.router = express.Router().post('/verify', this.verify);
+    this.settings = settings;
   }
 
-  create = async (request: Request, response: Response): Promise<void> => {
-    const message = await cryptoRandomString.async({ length: 32 });
-    // Should we ensure the random string doesn't exist in DB?
-    response.send({
-      message,
-    });
-  };
-
   verify = async (request: Request, response: Response): Promise<void> => {
-    const { message, signature } = request.body;
-    const walletAuth = await WalletAuth.findOne({ message });
+    const { verifiedTime, signature } = request.body;
 
-    if (!walletAuth) {
-      response.status(400).send({ error: 'Could not find message to verify signature.' });
+    const currentTime = Math.floor(Date.now() / 1000);
+    if (currentTime > verifiedTime + 30) {
+      response.status(400).send({ error: 'Signed timestamp has expired (30 second timeout).' });
       return;
     }
 
     try {
-      const address = await ethers.utils.verifyMessage(message, signature);
-      walletAuth.address = address;
-      walletAuth.verified = true;
-      await walletAuth.save();
-      response.send({ address });
+      const address = await ethers.utils.verifyMessage(verifiedTime, signature);
+      const tokenPrivateKey = process.env.AUTH_PRIVATE_KEY;
+      const exp = Math.floor(Date.now() / 1000) + this.settings.auth.tokenExpiry;
+      const token = sign({ exp, userId: address }, tokenPrivateKey);
+      response.status(201).send({ token });
     } catch (error) {
       response.status(400).send({ error: 'Could not verify message and signature.' });
     }
