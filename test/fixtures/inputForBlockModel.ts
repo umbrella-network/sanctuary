783760456ea017ab8ff12f90c9836aa47d09f257
Federico Rinaldi <fed@hey.com>
2021-03-28 16:23:33 +0100

    Fixed unit test

--- test/fixtures/inputForBlockModel.ts
+++ test/fixtures/inputForBlockModel.ts
@@ -7,6 +7,7 @@ export const inputForBlockModel: CreateQuery<IBlock> = {
   height: 1,
   minter: '0xA405324F4b6EB7Bc76f1964489b3769cfc71445F',
   numericFcdKeys: ['ETH-USD'],
+  numericFcdValues: [1700.5632],
   power: '1000000000000000000',
   // correct root hash if ETH-USD === '0x' + LeafValueCoder.encode(100, LeafType.TYPE_FLOAT).toString('hex')
   root: '0xbc0c5f121772154318bb0300d7a54affeabe880cd746f291da84c8b9312c25a9',
