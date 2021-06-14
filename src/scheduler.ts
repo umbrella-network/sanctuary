fb4aa12671a687d7d90c2f58407f00eb540799f2
Federico M. Rinaldi <8193701+federicorinaldi@users.noreply.github.com>
2021-06-14 15:44:10 +0100

    Feature/add minted blocks report (#74)
    * feat: adding reporting of last block minted time
    
    * feat: add new worker to send metrics to NR
    
    * fix: PR Review changes
    
    Co-authored-by: Federico Rinaldi <fed@hey.com>

--- src/scheduler.ts
+++ src/scheduler.ts
@@ -2,12 +2,18 @@ import './boot';
 import Application from './lib/Application';
 import BlockSynchronizerWorker from './workers/BlockSynchronizerWorker';
 import BlockResolverWorker from './workers/BlockResolverWorker';
+import MetricsWorker from './workers/MetricsWorker';
 import Settings from './types/Settings';
 
 (async (): Promise<void> => {
   const settings: Settings = Application.get('Settings');
   const blockSynchronizerWorker = Application.get(BlockSynchronizerWorker);
   const blockResolverWorker = Application.get(BlockResolverWorker);
+  const metricsWorker = Application.get(MetricsWorker);
+
+  setInterval(async () => {
+    await metricsWorker.enqueue({});
+  }, settings.jobs.metricsReporting.interval);
 
   setInterval(async () => {
     await blockSynchronizerWorker.enqueue({});
