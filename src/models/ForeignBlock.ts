0b039b73691efe035084d612fb945eda626b5d8c
DZariusz <dzariusz@gmail.com>
2021-08-26 12:56:35 +0200

    IForeignBlock.blockId

--- src/models/ForeignBlock.ts
+++ src/models/ForeignBlock.ts
@@ -1,5 +1,4 @@
 import mongoose, { Schema, Document } from 'mongoose';
-import { number } from 'yargs';
 
 export interface IForeignBlock extends Document {
   parentId: mongoose.Types.ObjectId;
@@ -7,17 +6,21 @@ export interface IForeignBlock extends Document {
   anchor: number;
   createdAt: Date;
   updatedAt: Date;
+  blockId: number;
 }
 
 const ForeignBlockSchema = new Schema({
   _id: { type: String, required: true },
   parentId: { type: mongoose.Types.ObjectId, required: true },
   anchor: { type: Number, required: true },
-  foreignChainId: { type: String, required: true }
+  foreignChainId: { type: String, required: true },
+  blockId: { type: Number, required: true, unique: true },
 }, {
   timestamps: true
-})
+});
 
 ForeignBlockSchema.index({ parentId: 1 });
-
+ForeignBlockSchema.index({ blockId: -1 });
+ForeignBlockSchema.index({ blockId: 1 });
+ForeignBlockSchema.index({ status: 1 });
 export default mongoose.model<IForeignBlock>('ForeignBlock', ForeignBlockSchema);
