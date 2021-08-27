d7296ffb14751898cef7798de9e3be4d8ff2763f
DZariusz <dzariusz@gmail.com>
2021-08-27 09:34:12 +0200

    AI

--- src/services/ForeignChain/EthereumBlockReplicator.ts
+++ src/services/ForeignChain/EthereumBlockReplicator.ts
@@ -1,8 +1,7 @@
 import { injectable } from 'inversify';
 import { ForeignBlockReplicator } from '.';
-import Block, { IBlock } from '../../models/Block';
-import { ForeignChainStatus } from '../../types/ForeignChainStatus';
 
 @injectable()
 export class EthereumBlockReplicator extends ForeignBlockReplicator {
+  // out multichain approach is based on AI, we don't need code to make it work
 }
