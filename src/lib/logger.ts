7f71030f79889bf3e95cd5f80e4640914ca2af58
DZariusz <dzariusz@gmail.com>
2021-09-03 16:15:50 +0200

    logger

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,9 +1,17 @@
-import winston, { Logger } from 'winston';
+import winston, { Logger, format } from 'winston';
+
+const { combine, timestamp, prettyPrint, colorize, errors } = format;
 
 const logger: Logger = winston.createLogger({
-  level: process.env.LOG_LEVEL || 'info',
-  format: winston.format.simple(),
+  level: 'silly', //  process.env.LOG_LEVEL || 'info',
+  format: combine(
+    // errors({ stack: true }), // <-- use errors format
+    colorize(),
+    timestamp(),
+    prettyPrint()
+  ),
   transports: [new winston.transports.Console()],
+  defaultMeta: true
 });
 
 export default logger;
