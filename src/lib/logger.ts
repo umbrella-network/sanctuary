f4846ae3342272f184a1fcd4d1da81d59d92b418
DZariusz <dzariusz@gmail.com>
2021-09-03 16:32:20 +0200

    logs are not pretty

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,14 +1,13 @@
 import winston, { Logger, format } from 'winston';
 
-const { combine, timestamp, prettyPrint, colorize, errors } = format;
+const { combine, timestamp, colorize } = format;
 
 const logger: Logger = winston.createLogger({
   level: process.env.LOG_LEVEL || 'info',
   format: combine(
     // errors({ stack: true }), // <-- use errors format
     colorize(),
-    timestamp(),
-    prettyPrint()
+    timestamp()
   ),
   transports: [new winston.transports.Console()],
   defaultMeta: true
