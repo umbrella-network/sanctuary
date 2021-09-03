cbb31a167ef43147cbda5671d45529168ca13f02
DZariusz <dzariusz@gmail.com>
2021-09-03 15:25:04 +0200

    lets not stop workers if some is not set up

--- src/lib/Blockchain.ts
+++ src/lib/Blockchain.ts
@@ -1,9 +1,11 @@
 import { inject, injectable } from 'inversify';
 import Settings, {BlockchainSettings} from '../types/Settings';
 import {ethers, Wallet} from 'ethers';
+import {Logger} from "winston";
 
 @injectable()
 class Blockchain {
+  @inject('Logger') protected logger!: Logger;
   settings!: Settings;
   providers: Record<string, ethers.providers.Provider> = {};
   wallets: Record<string, Wallet> = {};
@@ -12,14 +14,19 @@ class Blockchain {
     this.settings = settings;
 
     Object.keys(settings.blockchain.foreignChain).forEach(key => {
-      const blockchainSettings = (<Record<string, BlockchainSettings>>settings.blockchain.foreignChain)[key];
+      try {
+        const blockchainSettings = (<Record<string, BlockchainSettings>>settings.blockchain.foreignChain)[key];
 
-      this.providers[key] = ethers.providers.getDefaultProvider(blockchainSettings.providerUrl);
+        this.providers[key] = ethers.providers.getDefaultProvider(blockchainSettings.providerUrl);
 
-      const {replicatorPrivateKey} = settings.blockchain;
-      
-      if (replicatorPrivateKey) {
-        this.wallets[key] = new Wallet(replicatorPrivateKey, this.providers[key]);
+        const {replicatorPrivateKey} = settings.blockchain;
+
+        if (replicatorPrivateKey) {
+          this.wallets[key] = new Wallet(replicatorPrivateKey, this.providers[key]);
+        }
+      } catch (e) {
+        // lets not stop workers if some is not set up
+        this.logger.error(e);
       }
     });
   }
