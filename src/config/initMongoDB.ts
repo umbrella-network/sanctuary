05f5ccd31c68bdc8767013a1861d9e5c40e5384f
marcelow <marcelow@internet>
2021-09-15 18:30:28 +0200

    fix: remove redundant mongoose settings

--- src/config/initMongoDB.ts
+++ src/config/initMongoDB.ts
@@ -2,8 +2,12 @@ import Settings from '../types/Settings';
 import mongoose from 'mongoose';
 
 export default async function initMongoDB(settings: Settings): Promise<void> {
-  mongoose.set('useNewUrlParser', true);
-  mongoose.set('useFindAndModify', false);
-  mongoose.set('useCreateIndex', true);
-  await mongoose.connect(settings.mongodb.url, { useNewUrlParser: true, useUnifiedTopology: true });
+  await mongoose.connect(
+    settings.mongodb.url,
+    {
+      useNewUrlParser: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    });
 }
