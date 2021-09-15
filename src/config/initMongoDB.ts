00e104f4420bf0453a0d27e8f3c4a76ec7601686
marcelow <marcelow@internet>
2021-09-15 18:31:01 +0200

    fix: and make it pretty

--- src/config/initMongoDB.ts
+++ src/config/initMongoDB.ts
@@ -2,12 +2,10 @@ import Settings from '../types/Settings';
 import mongoose from 'mongoose';
 
 export default async function initMongoDB(settings: Settings): Promise<void> {
-  await mongoose.connect(
-    settings.mongodb.url,
-    {
-      useNewUrlParser: true,
-      useFindAndModify: false,
-      useUnifiedTopology: true,
-      useCreateIndex: true
-    });
+  await mongoose.connect(settings.mongodb.url, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  });
 }
