2163187d746c9faa691bd24d505d66feff869648
marcelow <marcelow@internet>
2021-09-15 16:59:45 +0200

    chore: fix mongoose deprecation warnings

--- src/config/initMongoDB.ts
+++ src/config/initMongoDB.ts
@@ -2,6 +2,8 @@ import Settings from '../types/Settings';
 import mongoose from 'mongoose';
 
 export default async function initMongoDB(settings: Settings): Promise<void> {
+  mongoose.set('useNewUrlParser', true);
   mongoose.set('useFindAndModify', false);
+  mongoose.set('useCreateIndex', true);
   await mongoose.connect(settings.mongodb.url, { useNewUrlParser: true, useUnifiedTopology: true });
 }
