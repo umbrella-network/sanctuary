bb8408a1e6f606352dcc9efdbfae2cd3b84cba84
DZariusz <dzariusz@gmail.com>
2021-09-24 00:35:46 +0200

    GAS

--- src/services/GasEstimator.ts
+++ src/services/GasEstimator.ts
@@ -1,30 +1,68 @@
-import { BlockWithTransactions } from '@ethersproject/abstract-provider';
-import { GasPriceMetrics } from '../types/GasPriceMetrics';
+import { BlockWithTransactions, FeeData } from '@ethersproject/abstract-provider';
 import { BigNumber, ethers } from 'ethers';
 
 // TODO move this to SDK
+
+type TxType2GasEstimation = {
+  baseFeePerGas: number;
+  gasPrice: number;
+  maxPriorityFeePerGas?: number | undefined;
+  maxFeePerGas?: number | undefined;
+};
+
+export interface GasEstimation extends TxType2GasEstimation {
+  isTxType2: boolean;
+  min: number;
+  max: number;
+  avg: number;
+}
+
+type EstimateParams = {
+  currentGasPrice: number;
+  feeData: FeeData;
+  minGasPrice: number;
+  maxGasPrice: number;
+  metrics: GasEstimation;
+  prices: number[];
+};
 
 export class GasEstimator {
   static async apply(
     provider: ethers.providers.Provider,
     minGasPrice: number,
     maxGasPrice: number
-  ): Promise<GasPriceMetrics> {
-    const block = await provider.getBlockWithTransactions('latest');
+  ): Promise<GasEstimation> {
+    const [block, currentGasPrice, feeData] = await Promise.all([
+      provider.getBlockWithTransactions('latest'),
+      provider.getGasPrice(),
+      provider.getFeeData(),
+    ]);
+
     const [metrics, prices] = GasEstimator.gasMetricsForBlock(block);
 
-    metrics.estimation = GasEstimator.estimate(minGasPrice, maxGasPrice, metrics, prices);
-
-    return metrics;
+    return GasEstimator.estimate({
+      currentGasPrice: currentGasPrice.toNumber(),
+      feeData,
+      metrics,
+      prices,
+      minGasPrice,
+      maxGasPrice,
+    });
   }
 
-  static printable(metrics: GasPriceMetrics): string {
-    return `estimation: ${GasEstimator.toGwei(metrics.estimation)} Gwei, min: ${GasEstimator.toGwei(
-      metrics.min
-    )} Gwei, max: ${GasEstimator.toGwei(metrics.max)} Gwei, avg: ${GasEstimator.toGwei(metrics.avg)} Gwei`;
+  static printable(metrics: GasEstimation): string {
+    return (
+      `gasPrice: ${GasEstimator.formatGwei(metrics.gasPrice)} Gwei, ` +
+      `maxPriorityFeePerGas: ${GasEstimator.formatGwei(metrics.maxPriorityFeePerGas)} Gwei, ` +
+      `maxFeePerGas: ${GasEstimator.formatGwei(metrics.maxFeePerGas)} Gwei, ` +
+      `min: ${GasEstimator.formatGwei(metrics.min)} Gwei, ` +
+      `max: ${GasEstimator.formatGwei(metrics.max)} Gwei, ` +
+      `avg: ${GasEstimator.formatGwei(metrics.avg)} Gwei`
+    );
   }
 
-  private static gasMetricsForBlock(block: BlockWithTransactions): [GasPriceMetrics, number[]] {
+  private static gasMetricsForBlock(block: BlockWithTransactions): [gasPriceMetrics: GasEstimation, prices: number[]] {
+    const isTxType2 = !!block.baseFeePerGas;
     let min = Number.MAX_VALUE;
     let max = 0;
     let sum = 0;
@@ -50,26 +88,50 @@ export class GasEstimator {
     });
 
     if (!prices.length) {
-      return [{ min: 0, max: 0, avg: 0, estimation: 0 }, []];
+      return [{ min: 0, max: 0, avg: 0, baseFeePerGas: 0, gasPrice: 0, isTxType2 }, []];
     }
 
-    return [{ min, max, avg: sum / prices.length, estimation: 0 }, prices];
+    return [
+      {
+        isTxType2,
+        min,
+        max,
+        avg: sum / prices.length,
+        gasPrice: 0,
+        baseFeePerGas: block.baseFeePerGas?.toNumber() || 0,
+      },
+      prices,
+    ];
   }
 
-  private static estimate = (
-    minGasPrice: number,
-    maxGasPrice: number,
-    metrics: GasPriceMetrics,
-    prices: number[]
-  ): number => {
-    if (prices.length < 2) {
-      return minGasPrice;
+  private static estimate = (params: EstimateParams): GasEstimation => {
+    console.log('params.prices.length', params.prices.length);
+
+    const minPrice = Math.min(Math.max(params.metrics.baseFeePerGas, params.minGasPrice), params.maxGasPrice);
+
+    if (params.prices.length < 2) {
+      return GasEstimator.makeGasEstimation(minPrice, params);
     }
 
-    const sortedPrices = prices.sort();
+    const gasPrice = params.metrics.isTxType2
+      ? minPrice
+      : GasEstimator.customEstimation(params.prices, params.minGasPrice, params.maxGasPrice);
+
+    console.log('params.metrics.isTxType2', params.metrics.isTxType2);
+    console.log('our estimation', GasEstimator.formatGwei(gasPrice));
+
+    console.log('maxFeePerGas', GasEstimator.formatGwei(params.feeData.maxFeePerGas.toNumber()));
+    console.log('gasPrice', GasEstimator.formatGwei(params.feeData.gasPrice.toNumber()));
+    console.log('maxPriorityFeePerGas', GasEstimator.formatGwei(params.feeData.maxPriorityFeePerGas.toNumber()));
+
+    return GasEstimator.makeGasEstimation(gasPrice, params);
+  };
+
+  private static customEstimation = (prices: number[], minGasPrice: number, maxGasPrice: number): number => {
+    const sortedPrices = prices.sort(); // from lower -> higher
     const bottomPrices = sortedPrices
-      .slice(0, Math.ceil((sortedPrices.length - 1) * 0.9))
-      .filter((p) => p >= minGasPrice);
+      .slice(0, Math.ceil((sortedPrices.length - 1) * 0.9)) // ignore top 10% of higher prices
+      .filter((p) => p >= minGasPrice); // ignore prices lower than our minimum
 
     let sum = 0;
     bottomPrices.forEach((p) => {
@@ -77,7 +139,7 @@ export class GasEstimator {
     });
 
     const avg = sum / bottomPrices.length;
-    const estimatedPrice = bottomPrices.filter((p) => p < avg).pop();
+    const estimatedPrice = bottomPrices.filter((p) => p < avg).pop(); // get price that is in a middle
 
     if (!estimatedPrice) {
       return minGasPrice;
@@ -86,5 +148,28 @@ export class GasEstimator {
     return Math.ceil(Math.min(maxGasPrice, Math.max(minGasPrice, estimatedPrice))) + 1;
   };
 
-  private static toGwei = (n: number): number => Math.round((n / 1e9) * 1e4) / 1e4;
+  private static makeGasEstimation(gasPrice: number, params: EstimateParams): GasEstimation {
+    const { isTxType2 } = params.metrics;
+    const maxPriorityFeePerGas = isTxType2 ? GasEstimator.calcMaxPriorityFeePerGas(params.feeData) : undefined;
+
+    return {
+      ...params.metrics,
+      gasPrice,
+      maxPriorityFeePerGas,
+      maxFeePerGas: isTxType2
+        ? GasEstimator.calcMaxFeePerGas(gasPrice, maxPriorityFeePerGas, params.maxGasPrice)
+        : undefined,
+    };
+  }
+
+  //  Doubling the Base Fee when calculating the Max Fee ensures that your transaction
+  //  will remain marketable for six consecutive 100% full blocks.
+  private static calcMaxFeePerGas = (baseFee: number, maxPriorityFee: number, maxGasPrice: number): number =>
+    Math.min(maxGasPrice, 2 * baseFee + maxPriorityFee);
+
+  private static calcMaxPriorityFeePerGas = (feeData: FeeData): number => feeData.maxPriorityFeePerGas?.toNumber() || 0;
+
+  private static formatGwei = (wei: number): number => Math.round((wei / 1e9) * 1e4) / 1e4;
+
+  private static wei = (gwei: number): number => gwei * 1e9;
 }
