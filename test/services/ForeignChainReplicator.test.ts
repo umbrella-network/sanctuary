0707b137a749e14f7266f9e8a905070b14abb20b
marcelow <marcelow@internet>
2021-09-27 15:03:14 +0200

    chore: clean up test

--- test/services/ForeignChainReplicator.test.ts
+++ test/services/ForeignChainReplicator.test.ts
@@ -53,69 +53,7 @@ describe('ForeignChainReplicator', () => {
     it('replicates blocks', async () => {
       const result = <IForeignBlock[]> await subject();
       expect(result.length).to.eq(1);
-      expect(result[0]._id).to.eq(foreignBlock._id);
-      // class Engine {
-      //   pistons: Piston[];
-      //
-      //   check = (): string[] => {
-      //     this.logCheckCall();
-      //     return this.pistons.map((p) => p.check());
-      //   }
-      //
-      //   checkWithMethod = (): string[] => this.pistons.map((p) => p.method());
-      //
-      //   private logCheckCall = () => console.log('check start');
-      // }
-      //
-      // class Piston {
-      //   check = (): string => 'Piston OK';
-      //   run = () => logger.info('Running');
-      //   method(): string { return 'METHOD OK'; }
-      // }
-      //
-      // // No Lib
-      // const engine = new Engine();
-      // const piston = <Piston><unknown> { check: (): string => 'MOCKED' };
-      // engine.pistons = [piston];
-      // console.log(engine.check());
-
-      // // TS Mockito
-      // const engine = new Engine();
-      // const PistonMock = mock(Piston);
-      // when(PistonMock.check).thenReturn((): string => 'MOCKED');
-      // const piston = instance(PistonMock);
-      // engine.pistons = [piston];
-      // console.log(engine.check());
-
-      // // Sinon Simple Stub - ONLY WORKS WITH METHODS!
-      // const engine = new Engine();
-      // const piston = sinon.createStubInstance(Piston);
-      // piston.method.returns('MOCKED');
-      // engine.pistons = [<Piston><unknown>piston];
-      // console.log(engine.checkWithMethod());
-
-      // // Sinon Arrow Stub
-      // const engine = new Engine();
-      // const piston = sinon.createStubInstance(Piston);
-      // piston.check = sinon.stub();
-      // piston.check.returns('MOCKED');
-      // engine.pistons = [<Piston><unknown>piston];
-      // console.log(engine.check());
-
-      // // TS-Sinon (Arrow)
-      // const engine = new Engine();
-      // const piston = stubConstructor(Piston);
-      // piston.check = sinon.stub();
-      // piston.check.returns('MOCKED');
-      // engine.pistons = [piston];
-      // console.log(engine.check());
-
-      // // TS-Sinon (Method)
-      // const engine = new Engine();
-      // const piston = stubObject<Piston>(new Piston());
-      // piston.method.returns('MOCKED');
-      // engine.pistons = [piston];
-      // console.log(engine.checkWithMethod());
+      expect(result[0]).to.eq(foreignBlock);
     });
   });
 });
