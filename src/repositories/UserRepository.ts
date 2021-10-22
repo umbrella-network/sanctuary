d6982c5d06e1506a5899feb1f2415df37aab7c3f
marcelow <marcelow@internet>
2021-10-22 11:07:42 +0200

    chore: adds undefined filtering

--- src/repositories/UserRepository.ts
+++ src/repositories/UserRepository.ts
@@ -3,6 +3,7 @@ import Settings from '../types/Settings';
 import { User } from '../types/User';
 import { ManagementClient, User as Auth0User } from 'auth0';
 import { Logger } from 'winston';
+import { isUndefined, omitBy } from 'lodash';
 
 export type FindProps = {
   id?: string;
@@ -46,12 +47,12 @@ export class UserRepository {
   }
 
   private deserialize(data: Auth0User): User {
-    return {
-      id: data._id,
+    return omitBy({
+      id: data._id, // TODO: check which ID this is
       email: data.email,
       name: data.name,
       createdAt: data.created_at && new Date(data.created_at),
       updatedAt: data.updated_at && new Date(data.updated_at)
-    };
+    }, isUndefined);
   }
 }
