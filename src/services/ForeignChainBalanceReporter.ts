70620c2a28e73ed2b2ebfca33b2a236ac7ce4097
Pedro Ramos <pedro.henrique@codeminer42.com>
2021-12-15 14:36:52 -0300

    Use Promise.allSettled on foreign chain balance reporter
    Use Promise.allSettled on the balance fetch to avoid not reporting anything at all when some of them fails. Add logging to failed fetches

--- src/services/ForeignChainBalanceReporter.ts
+++ src/services/ForeignChainBalanceReporter.ts
@@ -3,11 +3,17 @@ import { ChainCurrencyEnum } from '../types/ChainCurrency';
 import { ChainsIds as ChainId, ForeignChainsIds } from '../types/ChainsIds';
 import BalanceReporter, { BlockReport } from './BalanceReporter';
 
+interface PromiseResult {
+  status: 'fulfilled' | 'rejected';
+  value?: BlockReport;
+  reason?: any;
+}
 @injectable()
 class ForeignChainBalanceReporter extends BalanceReporter {
   async call(chainsIds: ChainId[] = ForeignChainsIds): Promise<void> {
-    const replicatorsBalances = await Promise.all(chainsIds.map(this.fetchBalanceOfReplicator));
-    this.reportBalances(replicatorsBalances);
+    const replicatorsBalances = await Promise.allSettled(chainsIds.map(this.fetchBalanceOfReplicator));
+    this.reportBalances(this.filterResolved(replicatorsBalances));
+    this.logRejectedResults(this.filterRejected(replicatorsBalances));
   }
 
   private fetchBalanceOfReplicator = async (chainId: ChainId): Promise<BlockReport> => {
@@ -22,6 +28,20 @@ class ForeignChainBalanceReporter extends BalanceReporter {
       currency: ChainCurrencyEnum[chainId],
     };
   };
+
+  private filterResolved(results: PromiseResult[]): BlockReport[] {
+    return results.filter((result) => result.status === 'fulfilled').map((result) => result.value);
+  }
+
+  private filterRejected(results: PromiseResult[]) {
+    return results.filter((result) => result.status === 'rejected').map((result) => result.reason);
+  }
+
+  private logRejectedResults(reports: BlockReport[]): void {
+    if (reports.length > 0) {
+      this.logger.error(`[ForeignChainBalanceReporter] failed to fetch data from foreing chains ${reports}`);
+    }
+  }
 }
 
 export default ForeignChainBalanceReporter;
