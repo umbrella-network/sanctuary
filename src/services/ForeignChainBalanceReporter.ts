85b072ffd740257f2a26b3c98195fd55e0319fa2
Pedro Ramos <pedro.henrique@codeminer42.com>
2021-12-16 09:48:16 -0300

    Improve promise results filter

--- src/services/ForeignChainBalanceReporter.ts
+++ src/services/ForeignChainBalanceReporter.ts
@@ -3,17 +3,21 @@ import { ChainCurrencyEnum } from '../types/ChainCurrency';
 import { ChainsIds as ChainId, ForeignChainsIds } from '../types/ChainsIds';
 import BalanceReporter, { BlockReport } from './BalanceReporter';
 
+type PromiseResultStatus = 'fulfilled' | 'rejected';
+
 interface PromiseResult {
-  status: 'fulfilled' | 'rejected';
+  status: PromiseResultStatus;
   value?: BlockReport;
-  reason?: any;
+  reason?: string;
 }
+
 @injectable()
 class ForeignChainBalanceReporter extends BalanceReporter {
   async call(chainsIds: ChainId[] = ForeignChainsIds): Promise<void> {
     const replicatorsBalances = await Promise.allSettled(chainsIds.map(this.fetchBalanceOfReplicator));
-    this.reportBalances(this.filterResolved(replicatorsBalances));
-    this.logRejectedResults(this.filterRejected(replicatorsBalances));
+    const { resolved, rejected } = this.filterResults(replicatorsBalances);
+    this.reportBalances(resolved);
+    this.logRejectedResults(rejected);
   }
 
   private fetchBalanceOfReplicator = async (chainId: ChainId): Promise<BlockReport> => {
@@ -29,17 +33,21 @@ class ForeignChainBalanceReporter extends BalanceReporter {
     };
   };
 
-  private filterResolved(results: PromiseResult[]): BlockReport[] {
-    return results.filter((result) => result.status === 'fulfilled').map((result) => result.value);
+  private filterResults(results: PromiseResult[]) {
+    const successful = results.filter(({ status }) => status === 'fulfilled').map(({ value }) => value);
+    const rejected = results
+      .filter(({ status }) => status === 'rejected')
+      .map(({ reason }) => reason)
+      .join(',');
+    return {
+      resolved: successful as BlockReport[],
+      rejected: rejected as string,
+    };
   }
 
-  private filterRejected(results: PromiseResult[]) {
-    return results.filter((result) => result.status === 'rejected').map((result) => result.reason);
-  }
-
-  private logRejectedResults(reports: BlockReport[]): void {
-    if (reports.length > 0) {
-      this.logger.error(`[ForeignChainBalanceReporter] failed to fetch data from foreing chains ${reports}`);
+  private logRejectedResults(rejectionMessages: string): void {
+    if (rejectionMessages) {
+      this.logger.error(`[ForeignChainBalanceReporter] failed to fetch data from foreing chains ${rejectionMessages}`);
     }
   }
 }
