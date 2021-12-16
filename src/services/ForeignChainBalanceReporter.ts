c256ca92cfec68c8a22200e76acf6df062988e90
Pedro Ramos <pedro.henrique@codeminer42.com>
2021-12-16 14:26:41 -0300

    hotifx: 4.7.1 (#129)
    * Use Promise.allSettled on foreign chain balance reporter
    
    Use Promise.allSettled on the balance fetch to avoid not reporting anything at all when some of them fails. Add logging to failed fetches
    
    * Update version and changelog
    
    * Improve promise results filter

--- src/services/ForeignChainBalanceReporter.ts
+++ src/services/ForeignChainBalanceReporter.ts
@@ -3,11 +3,21 @@ import { ChainCurrencyEnum } from '../types/ChainCurrency';
 import { ChainsIds as ChainId, ForeignChainsIds } from '../types/ChainsIds';
 import BalanceReporter, { BlockReport } from './BalanceReporter';
 
+type PromiseResultStatus = 'fulfilled' | 'rejected';
+
+interface PromiseResult {
+  status: PromiseResultStatus;
+  value?: BlockReport;
+  reason?: string;
+}
+
 @injectable()
 class ForeignChainBalanceReporter extends BalanceReporter {
   async call(chainsIds: ChainId[] = ForeignChainsIds): Promise<void> {
-    const replicatorsBalances = await Promise.all(chainsIds.map(this.fetchBalanceOfReplicator));
-    this.reportBalances(replicatorsBalances);
+    const replicatorsBalances = await Promise.allSettled(chainsIds.map(this.fetchBalanceOfReplicator));
+    const { resolved, rejected } = this.filterResults(replicatorsBalances);
+    this.reportBalances(resolved);
+    this.logRejectedResults(rejected);
   }
 
   private fetchBalanceOfReplicator = async (chainId: ChainId): Promise<BlockReport> => {
@@ -22,6 +32,24 @@ class ForeignChainBalanceReporter extends BalanceReporter {
       currency: ChainCurrencyEnum[chainId],
     };
   };
+
+  private filterResults(results: PromiseResult[]) {
+    const successful = results.filter(({ status }) => status === 'fulfilled').map(({ value }) => value);
+    const rejected = results
+      .filter(({ status }) => status === 'rejected')
+      .map(({ reason }) => reason)
+      .join(',');
+    return {
+      resolved: successful as BlockReport[],
+      rejected: rejected as string,
+    };
+  }
+
+  private logRejectedResults(rejectionMessages: string): void {
+    if (rejectionMessages) {
+      this.logger.error(`[ForeignChainBalanceReporter] failed to fetch data from foreing chains ${rejectionMessages}`);
+    }
+  }
 }
 
 export default ForeignChainBalanceReporter;
