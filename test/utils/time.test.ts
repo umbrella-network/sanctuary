28f880c02c59c4620ee77198cfdec4c6cbe6679a
Matheus Hahn <hahnnmatheus@gmail.com>
2022-01-31 16:37:45 -0300

    test: add #getDateAtMidnight describe

--- test/utils/time.test.ts
+++ test/utils/time.test.ts
@@ -2,10 +2,12 @@ import { expect } from 'chai';
 import { getDateAtMidnight } from '../../src/utils/time';
 
 describe('Time', () => {
-  describe('When a valid date is given', () => {
-    it('should return the given date with time equals 00:00:00', () => {
-      const day = getDateAtMidnight('2022-01-01');
-      expect(day).to.eqls('2022-01-01T00:00:00Z');
+  describe('#getDateAtMidnight', () => {
+    describe('When a valid date is given', () => {
+      it('should return the given date with time equals 00:00:00', () => {
+        const day = getDateAtMidnight('2022-01-01');
+        expect(day).to.eqls('2022-01-01T00:00:00Z');
+      });
     });
   });
 });
