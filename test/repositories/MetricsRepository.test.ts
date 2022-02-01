ab257bed5e2d42cc81ae3284ec6e58fc848d9323
Matheus Hahn <hahnnmatheus@gmail.com>
2022-02-01 09:29:21 -0300

    test: create new block in MetricsRepository.test.ts

--- test/repositories/MetricsRepository.test.ts
+++ test/repositories/MetricsRepository.test.ts
@@ -4,15 +4,21 @@ import { expect } from 'chai';
 import { MetricsRepository } from '../../src/repositories/MetricsRepository';
 import { getTestContainer } from '../helpers/getTestContainer';
 import { setupDatabase, teardownDatabase } from '../helpers/databaseHelpers';
+import Block from '../../src/models/Block';
+import { inputForBlockModel } from '../fixtures/inputForBlockModel';
+import addDays from 'date-fns/addDays';
 
 describe('MetricsRepository', () => {
   let metricsRepository: MetricsRepository;
 
   before(async () => {
     await setupDatabase();
+    await Block.deleteOne({ _id: 'block::1' });
+    await Block.create([{ ...inputForBlockModel, _id: 'block::1', blockId: 1, status: 'new' }]);
   });
 
   after(async () => {
+    await Block.deleteOne({ _id: 'block::1' });
     await teardownDatabase();
   });
 
@@ -24,15 +30,16 @@ describe('MetricsRepository', () => {
 
   describe('#getVotersCount', () => {
     it('returns only the voter count', async () => {
+      const endDateFormat = addDays(inputForBlockModel.dataTimestamp, 1);
       const votersCount = await metricsRepository.getVotersCount({
-        startDate: new Date('2021-12-01'),
-        endDate: new Date('2021-12-02'),
+        startDate: inputForBlockModel.dataTimestamp,
+        endDate: endDateFormat,
       });
 
       expect(votersCount).to.be.an('array');
       votersCount.forEach((voter) => {
-        expect(voter._id).to.be.a('string');
-        expect(voter.count).to.be.a('number');
+        expect(voter._id).to.be.equal('0xA405324F4b6EB7Bc76f1964489b3769cfc71445F');
+        expect(voter.count).to.be.equal(1);
       });
     });
   });
