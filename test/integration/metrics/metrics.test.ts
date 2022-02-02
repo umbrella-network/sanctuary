c8d3a6a133a005a7e397749500c4795f73411b51
Matheus Hahn <hahnnmatheus@gmail.com>
2022-02-02 09:03:51 -0300

    test: create metrics/voters integration test

---
+++ test/integration/metrics/metrics.test.ts
@@ -0,0 +1,111 @@
+import 'reflect-metadata';
+import { expect } from 'chai';
+import Server from '../../../src/lib/Server';
+import { Container } from 'inversify';
+import { getContainer } from '../../../src/lib/getContainer';
+import { loadTestEnv } from '../../helpers';
+import request from 'supertest';
+import { Application } from 'express';
+import createJWKSMock, { JWKSMock } from 'mock-jwks';
+import { after } from 'mocha';
+import nock from 'nock';
+import { setupDatabase, teardownDatabase } from '../../helpers/databaseHelpers';
+
+describe('/metrics', async () => {
+  let container: Container;
+  let app: Application;
+
+  before(async () => {
+    loadTestEnv();
+    await setupDatabase();
+    container = getContainer();
+    app = container.get(Server).app;
+  });
+
+  after(async () => {
+    await teardownDatabase();
+    nock.restore();
+  });
+
+  afterEach(() => {
+    nock.cleanAll();
+  });
+  describe('/metrics/voters', async () => {
+    describe('GET /metrics/voters', async () => {
+      describe('when an invalid bearer token is provided', async () => {
+        it('responds with HTTP 401 Unauthorized', async () => {
+          const res = await request(app).get('/metrics/voters');
+          expect(res.status).to.eq(401);
+        });
+      });
+
+      describe('when a valid bearer token is provided', async () => {
+        let jwksMock: JWKSMock;
+        let accessToken: string;
+        const profile = {
+          user_id: 'USER_ID',
+          username: 'test.user',
+          name: 'Test User',
+          email: 'test.user@example.com',
+        };
+
+        beforeEach(async () => {
+          jwksMock = createJWKSMock('https://example.com/');
+          await jwksMock.start();
+
+          accessToken = jwksMock.token({
+            aud: 'TEST_AUDIENCE',
+            iss: 'https://example.com/',
+            sub: 'USER_ID',
+          });
+
+          nock('https://test.auth0.com').post('/oauth/token').reply(200, 'MANAGEMENT_API_ACCESS_TOKEN');
+          nock('https://test.auth0.com').get('/api/v2/users/USER_ID').reply(200, profile);
+        });
+
+        afterEach(async () => {
+          await jwksMock.stop();
+        });
+
+        describe('when a valid query param is provided', () => {
+          it('should response with status 200', async () => {
+            const res = await request(app)
+              .get('/metrics/voters?startDate=2021-12-01&endDate=2022-01-01')
+              .set('Authorization', `Bearer ${accessToken}`);
+
+            expect(res.status).to.eq(200);
+          });
+        });
+
+        describe('when an invalid query param is provided', () => {
+          it('should response with status 400 when missing startDate and endDate', async () => {
+            const res = await request(app).get('/metrics/voters').set('Authorization', `Bearer ${accessToken}`);
+
+            expect(res.status).to.eq(400);
+          });
+          it('should response with status 400 when missing startDate', async () => {
+            const res = await request(app)
+              .get('/metrics/voters?endDate=2022-01-01')
+              .set('Authorization', `Bearer ${accessToken}`);
+
+            expect(res.status).to.eq(400);
+          });
+          it('should response with status 400 when missing endDate', async () => {
+            const res = await request(app)
+              .get('/metrics/voters?startDate=2022-12-01')
+              .set('Authorization', `Bearer ${accessToken}`);
+
+            expect(res.status).to.eq(400);
+          });
+          it('should response with status 400 when invalid date is provided', async () => {
+            const res = await request(app)
+              .get('/metrics/voters?startDate=Invalid')
+              .set('Authorization', `Bearer ${accessToken}`);
+
+            expect(res.status).to.eq(400);
+          });
+        });
+      });
+    });
+  });
+});
