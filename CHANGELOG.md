32687dc770b50ef6c737beab31f5b6a2409dfae2
Matheus Hahn <hahnnmatheus@gmail.com>
2022-02-07 17:47:19 -0300

    test: fix block integration test
    test: fix profile integration test
    
    test: fix e2e blocks test
    
    test: fix e2e project test
    
    test: update helper to not use auth route
    
    feat: run e2e tests
    
    chore: create script to start server for e2e tests locally
    
    feat: update package-lock.json
    
    fix: update scripts commands to run e2e
    
    refactor: move project e2e test to be an integration test
    
    refactor: make teardownTestUser run in parallel
    
    test: make block e2e run against server
    
    chore: update readme with unit and e2e tests command
    
    chore: update changelog
    
    test: init InfluxDB before start blocks e2e
    
    test: remove init influxDB from blocks e2e
    
    refactor: remove eslint-disable in getBlocks.e2e.ts
    
    feat: use a specific mongodb.url settings when testing
    
    test: create one expect per it
    
    refactor: set mongodb.url in test scripts
    
    test: remove server in e2e tests
    
    feat: move integrations tests to e2e folder
    
    refactor: remove start:e2e script
    
    chore: update changelog
    
    refactor: remove quotes
    
    refactor: remove unused code
    
    test: one expect per it
    
    refactor: update test.yml with test MONGODB_URL
    
    test: refactor blocks test
    
    chore: remove unnecessary echo in testing.env file
    
    refactor: rename authHarness to setupJWKSMock
    
    refactor: describe tests better
    
    refactor: remove check if value is object
    
    refactor: remove unnecessary config in readme and CI
    
    refactor: add new line in getBlocks.e2e.ts
    
    refactor: change descibe from getBlocks.e2e.ts
    
    refactor: add nl in e2e tests
    
    test: change getForeignBlocks test
    
    refactor: move createBlock to factory mock
    
    chore: add TODO and change readme
    
    fix: do not delete apiKey from different owner
    
    test: create more e2e test for delete apiKey
    
    fix: do not create apiKey from different owner
    
    test: create more e2e test for update apiKey
    
    refactor: change describe for createProject.e2e
    
    test: update interface of block in getBlocks.e2e
    
    test: create get block wrong id

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,13 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
+## Unreleased
+### Changed
+- Activate e2e tests in CI
+
+### Fixed
+- Do not delete apiKey from different owner.
+
 ## [4.9.0] - 2022-02-02
 ### Added
 - Create metrics/voters endpoint to get each voters vote count of given time interval
--- test/e2e/proofs/getProofs.e2e.ts
+++ test/e2e/proofs/getProofs.e2e.ts
@@ -1,29 +1,33 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import axios from 'axios';
+import 'reflect-metadata';
+import request from 'supertest';
+import { BlockStatus } from '@umb-network/toolbox/dist/types/BlockStatuses';
+import { expect } from 'chai';
+import { Container } from 'inversify';
+import { Application } from 'express';
+
 import { loadTestEnv } from '../../helpers';
-import chai, { expect } from 'chai';
-import chaiAsPromised from 'chai-as-promised';
 import Block, { IBlock } from '../../../src/models/Block';
-import Leaf from '../../../src/models/Leaf';
+import Leaf, { ILeaf } from '../../../src/models/Leaf';
 import { inputForBlockModel } from '../../fixtures/inputForBlockModel';
 import { setupDatabase, teardownDatabase } from '../../helpers/databaseHelpers';
-import { setupTestUser, teardownTestUser, TestUserHarness } from '../../helpers/authHelpers';
+import { setupJWKSMock, teardownTestUser, TestAuthHarness } from '../../helpers/authHelpers';
 import ForeignBlock, { IForeignBlock } from '../../../src/models/ForeignBlock';
 import { foreignBlockFactory } from '../../mocks/factories/foreignBlockFactory';
 import { blockFactory } from '../../mocks/factories/blockFactory';
-import { BlockStatus } from '@umb-network/toolbox/dist/types/BlockStatuses';
+import { getContainer } from '../../../src/lib/getContainer';
+import Server from '../../../src/lib/Server';
 
-chai.use(chaiAsPromised);
-
-describe('Getting proofs', () => {
-  let credentials: TestUserHarness;
-  const config = loadTestEnv();
-  const appAxios = axios.create({ baseURL: config.APP_URL });
+describe('getProofs', () => {
+  let container: Container;
+  let app: Application;
+  let authHarness: TestAuthHarness;
 
   before(async () => {
+    loadTestEnv();
     await setupDatabase();
-    await teardownTestUser();
-    credentials = await setupTestUser();
+
+    container = getContainer();
+    app = container.get(Server).app;
   });
 
   after(async () => {
@@ -31,135 +35,141 @@ describe('Getting proofs', () => {
     await teardownDatabase();
   });
 
-  describe('when no chain ID is provided', async () => {
-    beforeEach(async () => {
-      await Block.deleteMany({});
-      await Leaf.deleteMany({});
-    });
+  describe('GET /proofs', () => {
+    describe('when an invalid bearer token is provided', () => {
+      it('responds with HTTP 401 Unauthorized when no bearer token is given', async () => {
+        const res = await request(app).get('/proofs');
 
-    afterEach(async () => {
-      await Block.deleteMany({});
-      await Leaf.deleteMany({});
-    });
-
-    it('returns valid response without leaves for the latest finalized block when keys are not specified', async () => {
-      await Block.create([
-        { ...inputForBlockModel, _id: 'block::1', blockId: 1, status: 'new' },
-        { ...inputForBlockModel, _id: 'block::2', blockId: 2, status: 'finalized' },
-        { ...inputForBlockModel, _id: 'block::3', blockId: 3, status: 'failed' },
-        { ...inputForBlockModel, _id: 'block::4', blockId: 4, status: 'finalized' },
-      ]);
-
-      const proofsResponse = await appAxios.get('/proofs', {
-        headers: { authorization: `Bearer ${credentials.apiKey.key}` },
-      });
-      expect(proofsResponse.data).to.be.an('object');
-      expect(proofsResponse.data.data.block).to.be.an('object');
-      expect(proofsResponse.data.data.block).to.have.property('blockId', 4);
-      expect(proofsResponse.data.data.block).to.have.property('status', 'finalized');
-      expect(proofsResponse.data.data.keys).to.be.an('array').that.is.empty;
-      expect(proofsResponse.data.data.leaves).to.be.an('array').that.is.empty;
-    });
-
-    it('returns latest block with leaves matching specified keys', async () => {
-      await Block.create([
-        { ...inputForBlockModel, _id: 'block::1', blockId: 1 },
-        { ...inputForBlockModel, _id: 'block::2', blockId: 2 },
-        { ...inputForBlockModel, _id: 'block::3', blockId: 3 },
-        { ...inputForBlockModel, _id: 'block::4', blockId: 4 },
-      ]);
-
-      await Leaf.create([
-        { _id: 'leaf::block::1::a', blockId: 1, key: 'a', value: '0x0', proof: [] },
-        { _id: 'leaf::block::2::a', blockId: 2, key: 'a', value: '0x0', proof: [] },
-        { _id: 'leaf::block::3::a', blockId: 3, key: 'a', value: '0x0', proof: [] },
-        { _id: 'leaf::block::4::a', blockId: 4, key: 'a', value: '0x0', proof: [] },
-        { _id: 'leaf::block::4::b', blockId: 4, key: 'b', value: '0x0', proof: [] },
-        { _id: 'leaf::block::4::c', blockId: 4, key: 'c', value: '0x0', proof: [] },
-      ]);
-
-      const proofsResponse = await appAxios.get('/proofs', {
-        params: { keys: ['a', 'b'] },
-        headers: { authorization: `Bearer ${credentials.apiKey.key}` },
+        expect(res.status).to.eq(401);
       });
 
-      expect(proofsResponse.status).to.be.eq(200);
+      it('responds with HTTP 401 Unauthorized when no bearer token is wrong', async () => {
+        const res = await request(app).get('/proofs').set('Authorization', 'Bearer 123');
 
-      expect(proofsResponse.data).to.be.an('object');
-      expect(proofsResponse.data.data.block).to.be.an('object');
-      expect(proofsResponse.data.data.block).to.have.property('blockId', 4);
-
-      expect(proofsResponse.data.data.keys).to.be.an('array').deep.eq(['a', 'b']);
-      expect(proofsResponse.data.data.leaves).to.be.an('array').with.lengthOf(2);
-
-      const leafWithKeyA = proofsResponse.data.data.leaves.find((leaf: any) => leaf.key === 'a');
-      expect(leafWithKeyA).to.be.an('object').that.has.property('key', 'a');
-      expect(leafWithKeyA).to.be.an('object').that.has.property('blockId', 4);
-
-      const leafWithKeyB = proofsResponse.data.data.leaves.find((leaf: any) => leaf.key === 'b');
-      expect(leafWithKeyB).to.be.an('object').that.has.property('key', 'b');
-      expect(leafWithKeyB).to.be.an('object').that.has.property('blockId', 4);
+        expect(res.status).to.eq(401);
+      });
     });
 
-    it('returns empty object if no finalized block found', async () => {
-      await Block.create([
-        { ...inputForBlockModel, _id: 'block::1', blockId: 1, status: 'new' },
-        { ...inputForBlockModel, _id: 'block::2', blockId: 2, status: 'failed' },
-        { ...inputForBlockModel, _id: 'block::3', blockId: 3, status: 'failed' },
-        { ...inputForBlockModel, _id: 'block::4', blockId: 4, status: 'failed' },
-      ]);
-
-      const proofsResponse = await appAxios.get('/proofs', {
-        headers: { authorization: `Bearer ${credentials.apiKey.key}` },
+    describe('when a valid bearer token is provided', () => {
+      beforeEach(async () => {
+        authHarness = await setupJWKSMock();
       });
 
-      expect(proofsResponse.data).to.be.an('object');
-      expect(proofsResponse.data.data).to.be.an('object').that.is.empty;
-    });
-  });
-
-  describe('when a foreign Chain ID is provided', async () => {
-    let foreignBlock: IForeignBlock;
-    let block: IBlock;
-
-    const operation = async (chainId: string) =>
-      appAxios.get(`/proofs?chainId=${chainId}`, {
-        headers: {
-          authorization: `Bearer ${credentials.apiKey.key}`,
-        },
+      afterEach(async () => {
+        await authHarness.jwksMock.stop();
+        await Promise.all([Block.deleteMany({}), Leaf.deleteMany({}), ForeignBlock.deleteMany({})]);
       });
 
-    beforeEach(async () => {
-      await Block.deleteMany({});
-      await Leaf.deleteMany({});
-      await ForeignBlock.deleteMany({});
+      describe('when finalized block found', () => {
+        it('returns valid response without leaves for the latest finalized block', async () => {
+          await Block.create([
+            { ...inputForBlockModel, _id: 'block::1', blockId: 1, status: 'new' },
+            { ...inputForBlockModel, _id: 'block::2', blockId: 2, status: 'finalized' },
+            { ...inputForBlockModel, _id: 'block::3', blockId: 3, status: 'failed' },
+            { ...inputForBlockModel, _id: 'block::4', blockId: 4, status: 'finalized' },
+          ]);
 
-      foreignBlock = new ForeignBlock(foreignBlockFactory.build());
-      await foreignBlock.save();
+          const proofsResponse = await request(app)
+            .get('/proofs')
+            .set('Authorization', `Bearer ${authHarness.accessToken}`);
 
-      block = new Block(
-        blockFactory.build({
-          status: BlockStatus.Finalized,
-          blockId: foreignBlock.blockId,
-        })
-      );
+          expect(proofsResponse.body.data.block).to.have.property('blockId', 4);
+          expect(proofsResponse.body.data.block).to.have.property('status', 'finalized');
+          expect(proofsResponse.body.data.keys).to.be.an('array').that.is.empty;
+          expect(proofsResponse.body.data.leaves).to.be.an('array').that.is.empty;
+        });
+      });
 
-      await block.save();
-      const nonFinalizedBlock = new Block(blockFactory.build({ blockId: 1000, status: BlockStatus.New }));
-      await nonFinalizedBlock.save();
+      describe('when no finalized block found', () => {
+        it('returns empty object', async () => {
+          await Block.create([
+            { ...inputForBlockModel, _id: 'block::1', blockId: 1, status: 'new' },
+            { ...inputForBlockModel, _id: 'block::2', blockId: 2, status: 'failed' },
+            { ...inputForBlockModel, _id: 'block::3', blockId: 3, status: 'failed' },
+            { ...inputForBlockModel, _id: 'block::4', blockId: 4, status: 'failed' },
+          ]);
+
+          const proofsResponse = await request(app)
+            .get('/proofs')
+            .set('Authorization', `Bearer ${authHarness.accessToken}`);
+
+          expect(proofsResponse.body.data).to.be.an('object').that.is.empty;
+        });
+      });
     });
 
-    afterEach(async () => {
-      await Block.deleteMany({});
-      await Leaf.deleteMany({});
-      await ForeignBlock.deleteMany({});
+    describe('GET /proofs?keys=<n>', () => {
+      it('returns latest block with leaves matching specified keys', async () => {
+        await Block.create([
+          { ...inputForBlockModel, _id: 'block::1', blockId: 1 },
+          { ...inputForBlockModel, _id: 'block::2', blockId: 2 },
+          { ...inputForBlockModel, _id: 'block::3', blockId: 3 },
+          { ...inputForBlockModel, _id: 'block::4', blockId: 4 },
+        ]);
+
+        await Leaf.create([
+          { _id: 'leaf::block::1::a', blockId: 1, key: 'a', value: '0x0', proof: [] },
+          { _id: 'leaf::block::2::a', blockId: 2, key: 'a', value: '0x0', proof: [] },
+          { _id: 'leaf::block::3::a', blockId: 3, key: 'a', value: '0x0', proof: [] },
+          { _id: 'leaf::block::4::a', blockId: 4, key: 'a', value: '0x0', proof: [] },
+          { _id: 'leaf::block::4::b', blockId: 4, key: 'b', value: '0x0', proof: [] },
+          { _id: 'leaf::block::4::c', blockId: 4, key: 'c', value: '0x0', proof: [] },
+        ]);
+
+        const proofsResponse = await request(app)
+          .get('/proofs?keys=a&keys=b')
+          .set('Authorization', `Bearer ${authHarness.accessToken}`);
+        const leafWithKeyA = proofsResponse.body.data.leaves.find((leaf: ILeaf) => leaf.key === 'a');
+        const leafWithKeyB = proofsResponse.body.data.leaves.find((leaf: ILeaf) => leaf.key === 'b');
+
+        expect(proofsResponse.status).to.be.eq(200);
+        expect(proofsResponse.body.data.block).to.have.property('blockId', 4);
+        expect(proofsResponse.body.data.keys).to.be.an('array').deep.eq(['a', 'b']);
+        expect(proofsResponse.body.data.leaves).to.be.an('array').with.lengthOf(2);
+
+        expect(leafWithKeyA).to.be.an('object').that.has.property('key', 'a');
+        expect(leafWithKeyA).to.be.an('object').that.has.property('blockId', 4);
+
+        expect(leafWithKeyB).to.be.an('object').that.has.property('key', 'b');
+        expect(leafWithKeyB).to.be.an('object').that.has.property('blockId', 4);
+      });
     });
 
-    it('returns the latest finalized block', async () => {
-      const response = await operation('ethereum');
-      const subject = response.data.data.block;
-      expect(subject._id).to.eq(block._id);
-      expect(subject.blockId).to.eq(block.blockId);
+    describe('GET /proofs?chainId=<n>', () => {
+      describe('when a foreign Chain ID is provided', () => {
+        let foreignBlock: IForeignBlock;
+        let block: IBlock;
+
+        const operation = async (chainId: string) =>
+          request(app).get(`/proofs?chainId=${chainId}`).set('Authorization', `Bearer ${authHarness.accessToken}`);
+
+        beforeEach(async () => {
+          foreignBlock = new ForeignBlock(foreignBlockFactory.build());
+          block = new Block(
+            blockFactory.build({
+              status: BlockStatus.Finalized,
+              blockId: foreignBlock.blockId,
+            })
+          );
+          const nonFinalizedBlock = new Block(blockFactory.build({ blockId: 1000, status: BlockStatus.New }));
+
+          await foreignBlock.save();
+          await block.save();
+          await nonFinalizedBlock.save();
+        });
+
+        afterEach(async () => {
+          await Promise.all([Block.deleteMany({}), Leaf.deleteMany({}), ForeignBlock.deleteMany({})]);
+        });
+
+        it('returns the latest finalized block', async () => {
+          const response = await operation('ethereum');
+          const subject = response.body.data.block;
+
+          expect(subject._id).to.eq(block._id);
+          expect(subject.blockId).to.eq(block.blockId);
+        });
+      });
     });
   });
 });
