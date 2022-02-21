9049b246f6d712a464ff73dfbfc08eb1dd67566b
DZariusz <dzariusz@gmail.com>
2022-02-21 17:01:12 +0100

    fix: check latest foreign block id for proper chain

--- src/services/RevertedBlockResolver.ts
+++ src/services/RevertedBlockResolver.ts
@@ -20,19 +20,21 @@ class RevertedBlockResolver {
     this.logger.warn(`[${chainName}] Block reverted: from ${lastSubmittedBlockId} --> ${nextBlockId}`);
 
     const blockRes = chainId
-      ? await RevertedBlockResolver.revertForeignBlocks(nextBlockId, chainId)
-      : await RevertedBlockResolver.revertHomeBlocks(nextBlockId);
+      ? await this.revertForeignBlocks(nextBlockId, chainId)
+      : await this.revertHomeBlocks(nextBlockId);
 
     this.logger.info(`[${chainName}] because of reverts we deleted ${blockRes.deletedCount} blocks >= ${nextBlockId}`);
 
     return blockRes.deletedCount;
   }
 
-  private static async revertHomeBlocks(nextBlockId: number): Promise<DeleteWriteOpResultObject> {
+  private async revertHomeBlocks(nextBlockId: number): Promise<DeleteWriteOpResultObject> {
+    this.logger.warn(`[homeChain] deleting many: blockId gte ${nextBlockId}`);
     return Block.collection.deleteMany({ blockId: { $gte: nextBlockId } });
   }
 
-  private static async revertForeignBlocks(nextBlockId: number, chainId: string): Promise<DeleteWriteOpResultObject> {
+  private async revertForeignBlocks(nextBlockId: number, chainId: string): Promise<DeleteWriteOpResultObject> {
+    this.logger.warn(`[${chainId}] deleting many: blockId gte ${nextBlockId}`);
     return ForeignBlock.collection.deleteMany({ chainId: chainId, blockId: { $gte: nextBlockId } });
   }
 }
