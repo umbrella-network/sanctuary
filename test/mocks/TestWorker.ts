0528e4684a72f566aee78bdab9ae3650d1f91bcc
Matheus Hahn <hahnnmatheus@gmail.com>
2022-06-14 12:14:09 -0300

    chore: update README and fix BasicWorker and BlcokSynchronizer

--- test/mocks/TestWorker.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-import { injectable } from 'inversify';
-import BasicWorker from '../../src/workers/BasicWorker';
-import Bull from 'bullmq';
-
-@injectable()
-export class TestWorker extends BasicWorker {
-  apply = async (job: Bull.Job): Promise<void> => {};
-}
