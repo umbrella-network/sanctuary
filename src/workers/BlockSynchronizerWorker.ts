c2a05b718680e18cc87e03a778315e5e02c38a50
DZariusz <dzariusz@gmail.com>
2022-08-31 20:06:05 +0200

    blocksynchronizer??

--- src/workers/BlockSynchronizerWorker.ts
+++ src/workers/BlockSynchronizerWorker.ts
@@ -6,6 +6,7 @@ import BlockSynchronizer from '../services/BlockSynchronizer';
 import Settings from '../types/Settings';
 import ChainSynchronizer from '../services/ChainSynchronizer';
 import { SingletonWorker } from './SingletonWorker';
+import { ChainsIds } from '../types/ChainsIds';
 
 @injectable()
 class BlockSynchronizerWorker extends SingletonWorker {
@@ -15,24 +16,23 @@ class BlockSynchronizerWorker extends SingletonWorker {
   @inject(ChainSynchronizer) chainSynchronizer!: ChainSynchronizer;
 
   apply = async (job: Bull.Job): Promise<void> => {
-    const interval = this.settings.jobs.blockCreation.interval;
-    const lockTTL = this.settings.jobs.blockCreation.lockTTL;
-    if (this.isStale(job, interval)) return;
+    const { lockTTL, isStale, chainId } = this.parseJobData(job);
+    if (isStale) return;
 
-    await this.synchronizeWork('block-synchronizer', lockTTL, this.execute);
+    await this.synchronizeWork('block-synchronizer', lockTTL, () => this.execute(chainId));
   };
 
-  private execute = async (): Promise<void> => {
-    this.logger.info(`Running BlockSynchronizerWorker at ${new Date().toISOString()}`);
+  private execute = async (chainId: ChainsIds): Promise<void> => {
+    this.logger.info(`[${chainId}] Running BlockSynchronizerWorker at ${new Date().toISOString()}`);
 
     try {
-      await this.blockSynchronizer.apply();
+      await this.blockSynchronizer.apply(chainId);
     } catch (e) {
       newrelic.noticeError(e);
       this.logger.error(e);
     }
 
-    this.logger.info(`BlockSynchronizerWorker finished at ${new Date().toISOString()}`);
+    this.logger.info(`[${chainId}]BlockSynchronizerWorker finished at ${new Date().toISOString()}`);
   };
 }
 
