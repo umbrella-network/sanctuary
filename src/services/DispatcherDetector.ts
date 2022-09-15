1af8587e78209219477a927691491b6371314e9c
DZariusz <dzariusz@gmail.com>
2022-09-15 10:50:39 +0200

    homechain is dispatching

--- src/services/DispatcherDetector.ts
+++ src/services/DispatcherDetector.ts
@@ -1,12 +1,19 @@
-import { injectable } from 'inversify';
+import {inject, injectable} from 'inversify';
 
 import { NonEvmChainsIds, ChainsIds } from '../types/ChainsIds';
 import ChainInstance from '../models/ChainInstance';
+import Settings from '../types/Settings';
 
 @injectable()
 export class DispatcherDetector {
+  @inject('Settings') private readonly settings: Settings;
+
   apply = async (chainId: ChainsIds): Promise<boolean> => {
     try {
+      if (chainId == this.settings.blockchain.homeChain.chainId) {
+        return true;
+      }
+
       const nonEvm = NonEvmChainsIds.includes(chainId);
 
       if (nonEvm) {
