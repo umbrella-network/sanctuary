24cd682b3064aaa8f5b31f60a9518e09800a3424
DZariusz <dzariusz@gmail.com>
2024-02-12 21:14:05 +0100

    fix counting signatures by connect signing wallet with deviation wallet

--- src/workers/MetricsWorker.ts
+++ src/workers/MetricsWorker.ts
@@ -7,15 +7,18 @@ import { ChainsIds } from '../types/ChainsIds';
 import { STAKING_BANK_NAME, UMBRELLA_FEEDS_NAME } from '../constants/variables';
 import { OnChainTxFetcher } from '../services/on-chain-stats/OnChainTxFetcher';
 import { KeysUpdateService } from '../services/on-chain-stats/KeysUpdateService';
+import { ValidatorsWalletsScanner } from '../services/ValidatorsWalletsScanner';
 
 @injectable()
 class MetricsWorker extends BasicWorker {
   @inject(ContractSynchronizer) private contractSynchronizer!: ContractSynchronizer;
   @inject(OnChainTxFetcher) private onChainTxFetcher!: OnChainTxFetcher;
   @inject(KeysUpdateService) private keysUpdateService!: KeysUpdateService;
+  @inject(ValidatorsWalletsScanner) private validatorsWalletsScanner: ValidatorsWalletsScanner;
 
   apply = async (job: Bull.Job): Promise<void> => {
     this.logger.debug(`[MetricsWorker] apply for ${job.id}`);
+    const chains = Object.keys(this.settings.blockchain.blockchainScanner) as ChainsIds[];
 
     try {
       await this.keysUpdateService.apply();
@@ -24,7 +27,6 @@ class MetricsWorker extends BasicWorker {
     }
 
     try {
-      const chains = Object.keys(this.settings.blockchain.blockchainScanner) as ChainsIds[];
       await Promise.allSettled(chains.map((chainId) => this.syncOnChainTransactions(chainId)));
     } catch (e) {
       this.logger.error(`[MetricsWorker] syncOnChainTransactions: ${e.message}`);
@@ -37,7 +39,12 @@ class MetricsWorker extends BasicWorker {
       STAKING_BANK_NAME,
     ]);
 
-    await this.onChainTxFetcher.call(chainId, lastSyncedBlock);
+    if (lastSyncedBlock <= 0) return;
+
+    await Promise.all([
+      this.onChainTxFetcher.call(chainId, lastSyncedBlock),
+      this.validatorsWalletsScanner.call(chainId),
+    ]);
   };
 }
 
